2f08847f22545d0e770df190c86b169d249aa61a
AlbanoRavenPOGI <onlyven711@gmail.com>
2026-09-10 23:11:28 -0700

    answer item 4 - dbcontext responsibility

--- PREFINALS_EXAM/PREFINALS_EXAM/Controllers/HomeController.cs
+++ PREFINALS_EXAM/PREFINALS_EXAM/Controllers/HomeController.cs
@@ -22,6 +22,20 @@ namespace PREFINALS_EXAM.Controllers
                 new ExamItem { Id = 1, Category = "Relational Data Modeling & Model Binding", Question = "1. What is the main problem solved by using a database instead of an in-memory collection?", Options = new List<string>{ "A. It makes C# code shorter", "B. It prevents the application from restarting", "C. It allows data to persist after the application stops", "D. It removes the need for MVC" }, CorrectAnswer = 'C', AnswerText = "C. It allows data to persist after the application stops" },
                 new ExamItem { Id = 2, Category = "Relational Data Modeling & Model Binding", Question = "2. Which approach is being used when an existing database is used to generate EF Core entity classes?", Options = new List<string>{ "A. Code-First", "B. Database-First", "C. Model-First", "D. Controller-First" }, CorrectAnswer = 'B', AnswerText = "B. Database-First" },
                 new ExamItem { Id = 3, Category = "Relational Data Modeling & Model Binding", Question = "3. What is the primary purpose of Entity Framework Core?", Options = new List<string>{ "A. To create HTML pages automatically", "B. To replace the MVC Controller", "C. To map objects in code to relational database data", "D. To replace the C# compiler" }, CorrectAnswer = 'C', AnswerText = "C. To map objects in code to relational database data" },
+                new ExamItem { Id = 4, Category = "Relational Data Modeling & Model Binding", Question = "4. Which EF Core component is primarily responsible for communicating with the database?", Options = new List<string>{ "A. DbContext", "B. DbSetView", "C. ControllerContext", "D. RazorContext" }, CorrectAnswer = 'A', AnswerText = "A. DbContext" },
+
+
+
+
+
+
+
+
+
+
+
+
+
             };
         }
     }
